6993cdb938a63e03081da3b6b9538373ed49dc33
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-06 11:42:30 -0300

    Base do projeto

---
+++ script.js
@@ -0,0 +1,37 @@
+// Bibliotecas que nós instalamos manualmente
+const cheerio = require('cheerio');
+const request = require('request');
+
+// Bibliotecas nativas do Node.js
+const fs = require('fs');
+
+// URL do site
+const url = 'https://infosimples.com/vagas/desafio/commercia/product.html';
+
+// Objeto contendo a resposta final
+const respostaFinal = {};
+
+// Faz o request e manipula o corpo de resposta
+request(url, function (error, response, body) {
+    const parsedHtml = cheerio.load(body);
+
+    // Vamos pegar o título do produto, na tag H2, com ID "product_title"
+    respostaFinal['title'] = parsedHtml('h2#product_title').text();
+    respostaFinal['brand'] = parsedHtml('div.brand').text();
+    respostaFinal['categories'] = parsedHtml('.current-category a').text();
+       
+
+    // Aqui você adiciona os outros campos...
+    // Gera string JSON com a resposta final
+    const jsonRespostaFinal = JSON.stringify(respostaFinal);
+
+    // Salva o arquivo JSON com a resposta final
+    fs.writeFile('produto.json', jsonRespostaFinal, function (err) {
+        if (err) {
+            // Loga o erro (caso ocorra)
+            console.log(err);
+        } else {
+            console.log('Arquivo salvo com sucesso!');
+        }
+    });
+});
