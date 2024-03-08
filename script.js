46161b690061856a82e21f18216682d6e87dcaeb
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-08 11:22:17 -0300

    Campo Skuls feito

--- script.js
+++ script.js
@@ -15,16 +15,66 @@ const respostaFinal = {};
 request(url, function (error, response, body) {
     const parsedHtml = cheerio.load(body);
 
-    // Vamos pegar o título do produto, na tag H2, com ID "product_title"
+    // Pegando o título
     respostaFinal['title'] = parsedHtml('h2#product_title').text();
+
+    // Pegando o brand
     respostaFinal['brand'] = parsedHtml('div.brand').text();
     
+    // Pegando as categorias
     const categorias = []
     parsedHtml('.current-category>a').each((i, categoria) => {
         const categ = parsedHtml(categoria).text();
         categorias[i] = categ;
     });
-    respostaFinal['categorias'] = categorias;
+    respostaFinal['categories'] = categorias;
+
+    // Pegando a descrição
+    respostaFinal['description'] = parsedHtml('.proddet').text();
+
+    // Pegando os skus
+
+    // Vetor que armazena os objetos
+    let skus = [];
+    parsedHtml('.skus-area div>.card').each((i, s) =>{
+        
+        // Objeto dos skuls
+        let sku = {
+            "name": "",
+            "current_price": null,
+            "old_price": null,
+            "available": false
+        }
+
+        // Pegando as informações dos skuls
+        const name = parsedHtml(s).find('.card-container .prod-nome').text();
+        const current_price = parsedHtml(s).find('.card-container .prod-pnow').text();
+        const old_price = parsedHtml(s).find('.card-container .prod-pold').text();
+        
+        sku['name'] = name;                
+        
+        if(current_price == ''){
+            sku['current_price'] = null;
+        } else{
+            sku['current_price'] = current_price;
+        }
+
+        if(old_price == ''){
+            sku['old_price'] = null;
+        } else{
+            sku['old_price'] = old_price;
+        }
+
+        if(current_price == '' && current_price == ''){
+            sku['available'] = false;
+        } else{
+            sku['available'] = true;
+        }
+
+        skus.push(sku);
+    });
+
+    respostaFinal['skus'] = skus;
 
     // Aqui você adiciona os outros campos...
     // Gera string JSON com a resposta final
